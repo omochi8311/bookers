9e217a0387e0669d9f71dd0ca13aa6822a628f14
EC2 Default User <ec2-user@ip-172-31-42-101.ec2.internal>
2022-03-25 15:54:25 +0000

    課題のモデルを削除した後…

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
+
+  get 'books/new'
+  post 'books' => 'books#create'
   get 'books' => 'books#index'
+  get 'books/show'
+  get 'books/edit'
+  root to: 'homes#top'
+  get 'books' => 'books#new'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-git
